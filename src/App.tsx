96988643806a8df70a9948d3f2c6587a7a70797d
ofitoz93 <o.fitoz93@gmail.com>
2026-02-02 10:52:07 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://xvgdxasuekjtatecxzso.supabase.co';
